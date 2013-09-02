f9eb35a158910d480b764e688e5f0e3e4b298c64
Jonathan Commins <joncom@gmail.com>
2013-09-02 14:32:55 -0700

    Renamed variable.

--- interpolation.js
+++ interpolation.js
@@ -1,11 +1,11 @@
 ig.module('plugins.joncom.interpolation.interpolation')
 .defines(function(){
 
-    ig.Interpolation = function(start, end, duration, _easeFn) {
+    ig.Interpolation = function(start, end, duration, _easeFunction) {
         this.start = start || 0;
         this.end = end || 0;
         this.duration = duration || 0;
-        this._easeFn = _easeFn;
+        this._easeFunction = _easeFunction;
         this._startTime = ig.Timer.time;
         return this;
     };
@@ -16,8 +16,8 @@ ig.module('plugins.joncom.interpolation.interpolation')
         }
         var elapsed = ig.Timer.time - this._startTime
         var v = (this.duration - elapsed) / this.duration;
-        if (this._easeFn) {
-            v = this._easeFn(v);
+        if (this._easeFunction) {
+            v = this._easeFunction(v);
         }
         return (this.start * v) + (this.end * (1-v));
     };
